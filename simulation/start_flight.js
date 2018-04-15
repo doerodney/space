1f7e696f9a5bd43f579d40572694645b31c0fe74
doerodney <doerodney@gmail.com>
2018-04-14 22:51:23 -0400

    Modifications to flight script.

--- simulation/start_flight.js
+++ simulation/start_flight.js
@@ -1,46 +1,38 @@
-process = require('process');
+const process = require('process');
+const loc = require('./locations');
+
+
+// states
+const ascent = 0;
+const horizonal = 1;
+const descent = 2;
+const landing = 3;
+
+// velocity parameters (units per second)
+const vClimb = 5; 
+const vHorizontal = 40;
+const vDescent = 5;
+const vLanding = 0.5;
+
+function handleInterval(err, data) {
+
+
+
+}
+
+
 
 const from = process.argv[2] || 'HQ';
 const to = process.argv[3] || 'NE';
 
-const locations = {
-	'HQ': {
-		'x': 0,
-		'y': 0,
-		'z': 0
-	},
-	'NE': {
-		'x': 10000,
-		'y': 10000,
-		'z': 1000
-	},
-	'SE': {
-		'x': -10000,
-		'y': 10000,
-		'z': 2000
-	},
-	'SW': {
-		'x': -10000,
-		'y': -10000,
-		'z': 500
-	},
-	'NW': {
-		'x': -10000,
-		'y': 10000,
-		'z': 1500
-	}
-};
+const fromValid = loc.isValidLocation(from);
+const toValid = loc.isValidLocation(to);
+
+if (fromValid && toValid) {
+	const xyzFrom = loc.getLocation(from);
+	const xyzTo = loc.getLocation(to);
+
+  const ceiling = max(xzyFrom.z, xyzTo.z) + 400;
 
 
-if (from in locations) {
-	console.log('From: ' + from + ' (' + locations[from].x + ',' + locations[from].y + ',' + locations[from].z + ')');
-
-	if (to in locations) {
-		console.log('To: ' + to + ' (' + locations[to].x + ',' + locations[to].y + ',' + locations[to].z + ')');
-	} else {
-		console.log(to + ' is not  valid location.');
-	}
-
-} else {
-	console.log(from + ' is not a valid location.');
 }
