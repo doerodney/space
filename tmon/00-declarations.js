90822cdf9f83c114e4d61584689e381f7989dfd9
aru2 <rod.b.doe@nordstrom.com>
2018-04-17 18:59:52 -0700

    Added demo files on variable declarartions.

--- tmon/00-declarations.js
+++ tmon/00-declarations.js
@@ -1,12 +1,28 @@
+function getValue(condition) {
+    if (condition) {
+        var value = 'blue';
+        return value;
+    } else {
+        console.log('value = ', value);
+        return value;
+    }
+}
+
 console.log('Exploration of Declaration\n\n');
 
 console.log('color =', color);
 
+var color = getValue(true);
+console.log('color =', color);
+
+color = getValue(false);
+console.log('color =', color);
+
 const favorite = {name: 'cinnamon roll'};
-console.log('favorite is initially', favorite, '\n');
+console.log('favorite is initially:', favorite, '\n');
 
 favorite.name = 'beer';
-console.log('favorite is now', favorite, '\n');
+console.log('favorite is now:', favorite, '\n');
 
 for (var i = 0; i < 5; i++) {
    console.log('i =', i);
@@ -19,10 +35,10 @@ for (let x = 0; x < 5; x++) {
 console.log('\n');
 
 console.log('i =', i);
-console.log('x =', x);
+//console.log('x =', x);
 
-var color = 'rave green';
+color = 'rave green';
 
-favorite = {composer: 'Vivaldi'}
+//favorite = {composer: 'Vivaldi'}
 
 console.log(favorite);
